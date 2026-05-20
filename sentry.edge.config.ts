2adf0d017a229d80bab220da2ef785810e476f92
miyoung12 <miseja3@gmail.com>
2026-05-20 17:40:14 +0900

    feat: Sentry 에러 모니터링 SDK 도입

---
+++ sentry.edge.config.ts
@@ -0,0 +1,12 @@
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+    dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+
+    // Adds request headers and IP for users, for more info visit:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+    sendDefaultPii: true,
+    // that it will also get attached to your source maps
+    tracesSampleRate: 0.2,
+    debug: false,
+});
