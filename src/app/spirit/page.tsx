9660a4609670e6325e1c0b1c24bc11c818fd3f13
miyoung12 <miseja3@gmail.com>
2026-05-26 14:19:43 +0900

    feat: 스피릿 배경 트레일 효과 추가 및 카드 인터랙션 개선

--- src/app/spirit/page.tsx
+++ src/app/spirit/page.tsx
@@ -1,5 +1,5 @@
 import { STANDARD_QUESTIONS } from "./_data/surveyQuestions";
-import { SpiritStepClient3D } from "./test/_components/SpiritStepClient3D";
+import { SpiritStepClient3D } from "./_components/SpiritStepClient3D";
 
 export default function SpiritPage() {
   return <SpiritStepClient3D questions={STANDARD_QUESTIONS} />;
